60be4c5602b3173e661b2c192fe0d42f5205f25b
Nathaniel Lee <nlee@appnexus.com>
2017-02-16 14:39:58 -0500

    Added some new action types.

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -1,8 +1,21 @@
 import _ from 'lodash';
 
-export default _.reduce([
+const matchActionTypes = [
+	'ADD_WRESTLER_TO_MATCH',
+	'INCREMENT_ROUND_NUMBER',
+	'DECREMENT_ROUND_NUMBER',
+	'SET_MAX_ROUNDS',
+	'START_MATCH',
+];
+
+const rosterActionTypes = [
 	'ADD_WRESTLER',
 	'SET_WRESTLERS',
+];
+
+export default _.reduce([
+	...matchActionTypes,
+	...rosterActionTypes,
 ], (actionTypes, type) => ({
 	...actionTypes,
 	[type]: type,
