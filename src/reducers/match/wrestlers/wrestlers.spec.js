901fa6ac4c28125d7738620dd45330154842bafc
Nathaniel Lee <nlee@appnexus.com>
2017-02-17 17:58:26 -0500

    Extracted the action to a higher scope for reuse.

--- src/reducers/match/wrestlers/wrestlers.spec.js
+++ src/reducers/match/wrestlers/wrestlers.spec.js
@@ -3,6 +3,10 @@ import reducer, * as selectors from './wrestlers';
 
 describe('match.wrestlers reducer', () => {
 	const { ADD_WRESTLER_TO_MATCH } = actionTypes;
+	const addWrestlerAction = {
+		type: ADD_WRESTLER_TO_MATCH,
+		wrestler: { id: 2 },
+	};
 	let initialState;
 
 	beforeEach(() => {
@@ -14,30 +18,18 @@ describe('match.wrestlers reducer', () => {
 	});
 
 	test(`returns current state with "wrestler" from an "${ADD_WRESTLER_TO_MATCH}" action appended if its "id" is not already found in state.`, () => {
-		const action = {
-			type: ADD_WRESTLER_TO_MATCH,
-			wrestler: { id: 2 },
-		};
-		expect(reducer(initialState, action)).toEqual([
-			{ id: 2 },
+		expect(reducer(initialState, addWrestlerAction)).toEqual([
+			addWrestlerAction.wrestler,
 		]);
 	});
 
 	test(`returns current state if "wrestler" from an "${ADD_WRESTLER_TO_MATCH}" action has "id" that is already found in state.`, () => {
-		const addWrestlerAction = {
-			type: ADD_WRESTLER_TO_MATCH,
-			wrestler: { id: 3 },
-		};
 		const addedState = reducer(initialState, addWrestlerAction);
 
 		expect(reducer(addedState, addWrestlerAction)).toEqual(addedState);
 	});
 
 	test(`returns current state when it receives an unrecognized action.`, () => {
-		const addWrestlerAction = {
-			type: ADD_WRESTLER_TO_MATCH,
-			wrestler: { id: 5 },
-		};
 		const updatedState = reducer(initialState, addWrestlerAction);
 		const unrecognizedAction = { type: 'unrecognized action' };
 
