491355b22c227aa98779a8d3513638c37ddbd303
Nathaniel Lee <nlee@appnexus.com>
2017-02-17 17:57:49 -0500

    Extracted action to higher scope for reuse.

--- src/reducers/match/roundNumber/roundNumber.spec.js
+++ src/reducers/match/roundNumber/roundNumber.spec.js
@@ -3,10 +3,13 @@ import reducer from './roundNumber';
 
 describe('match.roundNumber reducer', () => {
 	const {
-		INCREMENT_ROUND_NUMBER,
 		DECREMENT_ROUND_NUMBER,
+		INCREMENT_ROUND_NUMBER,
 		START_MATCH,
 	} = actionTypes;
+	const decrementAction = { type: DECREMENT_ROUND_NUMBER };
+	const incrementAction = { type: INCREMENT_ROUND_NUMBER };
+	const startMatchAction = { type: START_MATCH };
 	let initialState;
 
 	beforeEach(() => {
@@ -18,31 +21,25 @@ describe('match.roundNumber reducer', () => {
 	});
 
 	test(`returns current state + 1 when it receives an "${INCREMENT_ROUND_NUMBER}" action.`, () => {
-		const action = { type: INCREMENT_ROUND_NUMBER };
-		const updatedState = reducer(initialState, action);
+		const updatedState = reducer(initialState, incrementAction);
 
 		expect(updatedState).toBe(initialState + 1);
-		expect(reducer(updatedState, action)).toBe(updatedState + 1);
+		expect(reducer(updatedState, incrementAction)).toBe(updatedState + 1);
 	});
 
 	test(`returns current state - 1 when it receives a "${DECREMENT_ROUND_NUMBER}" action.`, () => {
-		const action = { type: DECREMENT_ROUND_NUMBER };
-		const updatedState = reducer(initialState, action);
+		const updatedState = reducer(initialState, decrementAction);
 
 		expect(updatedState).toBe(initialState - 1);
-		expect(reducer(updatedState, action)).toBe(updatedState - 1);
+		expect(reducer(updatedState, decrementAction)).toBe(updatedState - 1);
 	});
 
 	test(`returns 1 when it receives a "${START_MATCH}" action.`, () => {
-		const incrementAction = { type: INCREMENT_ROUND_NUMBER };
 		const incrementedState = reducer(initialState, incrementAction);
-		const startMatchAction = { type: START_MATCH };
-
 		expect(reducer(incrementedState, startMatchAction)).toBe(1);
 	});
 
 	test(`returns current state when it receives an unrecognized action.`, () => {
-		const incrementAction = { type: INCREMENT_ROUND_NUMBER };
 		const incrementedState = reducer(initialState, incrementAction);
 		const unrecognizedAction = { type: 'unrecognized action' };
 
