5ac0748d92ca81750cd61a9095eca25c14985a1d
Nathaniel Lee <nlee@appnexus.com>
2017-02-17 17:56:56 -0500

    Move action to higher scope for reuse.

--- src/reducers/match/maxRounds/maxRounds.spec.js
+++ src/reducers/match/maxRounds/maxRounds.spec.js
@@ -3,6 +3,10 @@ import reducer from './maxRounds';
 
 describe('match.maxRounds reducer', () => {
 	const { SET_MAX_ROUNDS } = actionTypes;
+	const setMaxRoundsAction = {
+		type: SET_MAX_ROUNDS,
+		maxRounds: 10,
+	};
 	let initialState;
 
 	beforeEach(() => {
@@ -14,18 +18,10 @@ describe('match.maxRounds reducer', () => {
 	});
 
 	test(`returns "maxRounds" from a "${SET_MAX_ROUNDS}" action.`, () => {
-		const action = {
-			type: SET_MAX_ROUNDS,
-			maxRounds: 10,
-		};
-		expect(reducer(initialState, action)).toBe(10);
+		expect(reducer(initialState, setMaxRoundsAction)).toBe(setMaxRoundsAction.maxRounds);
 	});
 
 	test(`returns current state when it receives an unrecognized action.`, () => {
-		const setMaxRoundsAction = {
-			type: SET_MAX_ROUNDS,
-			maxRounds: 20,
-		};
 		const updatedState = reducer(initialState, setMaxRoundsAction);
 		const unrecognizedAction = { type: 'unrecognized action' };
 
