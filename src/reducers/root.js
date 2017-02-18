b75a0eb5685fb4f6f9aab5f0bfac4ccc554295e7
Nathaniel Lee <nlee@appnexus.com>
2017-02-18 11:39:37 -0500

    Exposed match selectors in root.

--- src/reducers/root.js
+++ src/reducers/root.js
@@ -10,5 +10,9 @@ const root = combineReducers({
 
 export default root;
 export const get = _.identity;
-export const getMatch = (state) => fromMatch.get(state.match);
+export const match = {
+	getMaxRounds: (state) => fromMatch.getMaxRounds(state.match),
+	getRoundNumber: (state) => fromMatch.getRoundNumber(state.match),
+	getWrestlers: (state) => fromMatch.getWrestlers(state.match),
+};
 export const getWrestlers = (state) => fromWrestlers.get(state.wrestlers);
