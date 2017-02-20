fb789252217eb0ba8233dbee0d458aef95be557b
Nathaniel Lee <nlee@appnexus.com>
2017-02-20 14:52:05 -0500

    Added components directory under src and moved the App component and related files into there.

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+export default React.createClass({
+  render() {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <h2>SnapSuplex Simulator</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  },
+});
