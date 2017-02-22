f1168a0718a86dbc3c09587d65aac662421ccfc2
Nathaniel Lee <nlee@appnexus.com>
2017-02-21 21:47:21 -0500

    Updated the App component to accept a fetchWrestlers function as a prop which it will call upon mounting.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,40 @@
+import _ from 'lodash';
 import React from 'react';
 import Content from './Content/Content';
 import Header from './Header/Header';
 import Nav from './Nav/Nav';
 import './App.css';
 
-export default ({ children }) => (
-  <div className='App'>
-    <Header />
-    <Nav />
-    <Content>
-      {children}
-    </Content>
-  </div>
-);
+const {
+	func,
+	node,
+} = React.PropTypes;
+
+export default React.createClass({
+	propTypes: {
+		children: node,
+		fetchWrestlers: func,
+	},
+
+	getDefaultProps() {
+		return {
+			fetchWrestlers: _.noop,
+		};
+	},
+
+	componentDidMount() {
+		this.props.fetchWrestlers();
+	},
+
+	render() {
+		return (
+			<div className='App'>
+				<Header />
+				<Nav />
+				<Content>
+					{this.props.children}
+				</Content>
+			</div>
+		);
+	},
+});
