29ce34ebc747e15f2fd75a08bc8fe7f40ccd05b7
Nathaniel Lee <nlee@appnexus.com>
2017-03-02 10:46:57 -0500

    Updated the dialog connection's map state to props to map a wrestler's move ids to moves from state.

--- src/components/Wrestlers/Dialog/ConnectedDialog.js
+++ src/components/Wrestlers/Dialog/ConnectedDialog.js
@@ -1,10 +1,25 @@
+import _ from 'lodash';
 import { connect } from 'react-redux';
 import { selectors } from '../../../reducers/root';
 import Dialog from './Dialog';
 
-const mapStateToProps = (state, ownProps) => ({
-	wrestler: selectors.getWrestler(state, ownProps.id),
-});
+const mapStateToProps = (state, { id }) => {
+	const wrestler = selectors.getWrestler(state, id);
+	const moveIdsByStat = wrestler.moves;
+	const movesByStat = _.reduce(moveIdsByStat, (results, moveIds, statKey) => {
+		return {
+			...results,
+			[statKey]: _.map(moveIds, (moveId) => selectors.getMove(state, moveId)),
+		};
+	}, {});
+
+	return {
+		wrestler: {
+			...wrestler,
+			moves: movesByStat,
+		},
+	};
+};
 
 const mapDispatchToProps = (dispatch, {
 	onCancel,
