6350906928a659ca23f4af6b14571f580dec8423
Nathaniel Lee <nlee@appnexus.com>
2017-03-01 22:07:07 -0500

    Added async action creators around fetch moves.

--- src/actions/fetch/createAsyncActionCreators.js
+++ src/actions/fetch/createAsyncActionCreators.js
@@ -1,6 +1,14 @@
 import actionCreators from './actionCreators';
 
 export default (api) => ({
+	fetchMoves: () => (dispatch) => {
+		dispatch(actionCreators.fetchMovesRequest());
+		return api.moves.get().then((moves) => {
+			dispatch(actionCreators.fetchMovesSuccess(moves));
+		}, (error) => {
+			dispatch(actionCreators.fetchMovesFailure(error));
+		});
+	},
 	fetchWrestlers: () => (dispatch) => {
 		dispatch(actionCreators.fetchWrestlersRequest());
 		return api.wrestlers.get().then((wrestlers) => {
