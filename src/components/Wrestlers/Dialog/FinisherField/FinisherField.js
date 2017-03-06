2bd9d47d8a47cff48ec5afd934a776c8c2d49ca4
Nathaniel Lee <nlee@appnexus.com>
2017-03-06 01:19:17 -0500

    Added FinisherField component to the app.

---
+++ src/components/Wrestlers/Dialog/FinisherField/FinisherField.js
@@ -0,0 +1,123 @@
+import _ from 'lodash';
+import {
+	Grid,
+	RadioGroup,
+	SingleSelect,
+	TextField,
+} from 'lucid';
+import React from 'react';
+import './FinisherField.css';
+
+const {
+	func,
+	number,
+	string,
+} = React.PropTypes;
+
+const statLabels = [
+	{
+		key: 'str',
+		label: 'Strength',
+	},
+	{
+		key: 'bra',
+		label: 'Brawling',
+	},
+	{
+		key: 'dex',
+		label: 'Dexterity',
+	},
+	{
+		key: 'tec',
+		label: 'Technical',
+	},
+];
+const levelRange = _.range(1, 5);
+
+export default React.createClass({
+	propTypes: {
+		description: string,
+		label: string,
+		level: number,
+		stat: string,
+		onChangeDescription: func,
+		onChangeLevel: func,
+		onChangeStat: func,
+	},
+
+	getDefaultProps() {
+		return {
+			description: '',
+			label: '',
+			level: 1,
+			stat: 'str',
+			onChangeDescription: _.noop,
+			onChangeLevel: _.noop,
+			onChangeStat: _.noop,
+		};
+	},
+
+	onChangeLevel(selectedIndex) {
+		this.props.onChangeLevel(levelRange[selectedIndex]);
+	},
+
+	onChangeStat(selectedIndex) {
+		this.props.onChangeStat(statLabels[selectedIndex].key);
+	},
+
+	render() {
+		const {
+			description,
+			label,
+			level,
+			stat,
+			onChangeDescription,
+		} = this.props;
+
+		return (
+			<div className='FinisherField'>
+				<Grid isGutterless>
+					<Grid.Cell is2>
+						<SingleSelect
+							selectedIndex={_.findIndex(statLabels, { key: stat })}
+							onSelect={this.onChangeStat}
+						>
+							{_.map(statLabels, ({ key, label }) => (
+								<SingleSelect.Option key={key}>
+									{label}
+								</SingleSelect.Option>
+							))}
+						</SingleSelect>
+					</Grid.Cell>
+					<Grid.Cell
+						className='FinisherField-description-input'
+						is5
+					>
+						<TextField
+							value={description}
+							onChange={onChangeDescription}
+						/>
+					</Grid.Cell>
+					<Grid.Cell className='FinisherField-level-label'>
+						Level:
+					</Grid.Cell>
+					<Grid.Cell
+						className='FinisherField-level-input'
+						is4
+					>
+						<RadioGroup
+							selectedIndex={_.indexOf(levelRange, level)}
+							onSelect={this.onChangeLevel}
+						>
+							{_.map(levelRange, (levelValue) => (
+								<RadioGroup.RadioButton key={levelValue}>
+									<RadioGroup.Label>{levelValue}</RadioGroup.Label>
+								</RadioGroup.RadioButton>
+							))}
+						</RadioGroup>
+					</Grid.Cell>
+				</Grid>
+			</div>
+		);
+	},
+});
