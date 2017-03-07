fcf6df2712eb20318e2e1378c138869ef50211a0
Nathaniel Lee <nlee@appnexus.com>
2017-03-06 19:01:02 -0500

    Updated the synchronous action creators.

--- src/actions/wrestlers/actionCreators.js
+++ src/actions/wrestlers/actionCreators.js
@@ -1,118 +1,44 @@
-import _ from 'lodash';
-import api from '../../utils/api';
-import createAsyncActionCreators from '../fetch/createAsyncActionCreators';
 import actionTypes from './actionTypes';
 
 export default {
-	addWrestler: (wrestler) => (dispatch) => {
-		const braMovePostPromises = _.map(wrestler.moves.bra, (move) => {
-			return api.moves.post(move);
-		});
-		const braMovePostsDonePromise = Promise.all(braMovePostPromises)
-			.then((braMoves) => ({ bra: _.map(braMoves, 'id') }));
-
-		const dexMovePostPromises = _.map(wrestler.moves.dex, (move) => {
-			return api.moves.post(move);
-		});
-		const dexMovePostsDonePromise = Promise.all(dexMovePostPromises)
-			.then((dexMoves) => ({ dex: _.map(dexMoves, 'id') }));
-
-		const strMovePostPromises = _.map(wrestler.moves.str, (move) => {
-			return api.moves.post(move);
-		});
-		const strMovePostsDonePromise = Promise.all(strMovePostPromises)
-			.then((strMoves) => ({ str: _.map(strMoves, 'id') }));
-
-		const tecMovePostPromises = _.map(wrestler.moves.tec, (move) => {
-			return api.moves.post(move);
-		});
-		const tecMovePostsDonePromise = Promise.all(tecMovePostPromises)
-			.then((tecMoves) => ({ tec: _.map(tecMoves, 'id') }));
-
-		const finisherPostPromises = _.map(wrestler.moves.fin, (move) => {
-			return api.moves.post(move);
-		});
-		const finisherPostsDonePromise = Promise.all(finisherPostPromises)
-			.then((finishers) => ({ fin: _.map(finishers, 'id') }));
-
-		Promise.all([
-			braMovePostsDonePromise,
-			dexMovePostsDonePromise,
-			strMovePostsDonePromise,
-			tecMovePostsDonePromise,
-			finisherPostsDonePromise,
-		]).then((moves) => {
-			const newWrestler = {
-				...wrestler,
-				moves: _.reduce(moves, (results, statMoves) => ({
-					...results,
-					...statMoves,
-				}), {}),
-			};
-			api.wrestlers.post(newWrestler).then(() => {
-				const asyncActionCreators = createAsyncActionCreators(api);
-				dispatch(asyncActionCreators.fetchMoves());
-				dispatch(asyncActionCreators.fetchWrestlers());
-			});
-		});
-	},
-
 	setWrestlers: (wrestlers) => ({
 		type: actionTypes.SET_WRESTLERS,
 		wrestlers,
 	}),
-
-	updateWrestler: (wrestler) => (dispatch) => {
-		// This is actually more of an edit wrestler. Need to adjust.
-		const braMovePostPromises = _.map(wrestler.moves.bra, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const braMovePostsDonePromise = Promise.all(braMovePostPromises)
-			.then((braMoves) => ({ bra: _.map(braMoves, 'id') }));
-
-		const dexMovePostPromises = _.map(wrestler.moves.dex, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const dexMovePostsDonePromise = Promise.all(dexMovePostPromises)
-			.then((dexMoves) => ({ dex: _.map(dexMoves, 'id') }));
-
-		const strMovePostPromises = _.map(wrestler.moves.str, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const strMovePostsDonePromise = Promise.all(strMovePostPromises)
-			.then((strMoves) => ({ str: _.map(strMoves, 'id') }));
-
-		const tecMovePostPromises = _.map(wrestler.moves.tec, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const tecMovePostsDonePromise = Promise.all(tecMovePostPromises)
-			.then((tecMoves) => ({ tec: _.map(tecMoves, 'id') }));
-
-		const finisherPostPromises = _.map(wrestler.moves.fin, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const finisherPostsDonePromise = Promise.all(finisherPostPromises)
-			.then((finishers) => ({ fin: _.map(finishers, 'id') }));
-
-		Promise.all([
-			braMovePostsDonePromise,
-			dexMovePostsDonePromise,
-			strMovePostsDonePromise,
-			tecMovePostsDonePromise,
-			finisherPostsDonePromise,
-		]).then((moves) => {
-			const updatedWrestler = {
-				...wrestler,
-				moves: _.reduce(moves, (results, statMoves) => ({
-					...results,
-					...statMoves,
-				}), {}),
-			};
-			api.wrestlers.put(updatedWrestler).then(() => {
-				const asyncActionCreators = createAsyncActionCreators(api);
-				dispatch(asyncActionCreators.fetchMoves());
-				dispatch(asyncActionCreators.fetchWrestlers());
-			});
-		});
-	},
+	postMoveRequest: () => ({ type: actionTypes.POST_MOVE_REQUEST }),
+	postMoveSuccess: (move) => ({
+		type: actionTypes.POST_MOVE_SUCCESS,
+		move,
+	}),
+	postMoveFailure: (error) => ({
+		type: actionTypes.POST_MOVE_FAILURE,
+		error,
+	}),
+	putMoveRequest: () => ({ type: actionTypes.PUT_MOVE_REQUEST }),
+	putMoveSuccess: (move) => ({
+		type: actionTypes.PUT_MOVE_SUCCESS,
+		move,
+	}),
+	putMoveFailure: (error) => ({
+		type: actionTypes.PUT_MOVE_FAILURE,
+		error,
+	}),
+	postWrestlerRequest: () => ({ type: actionTypes.POST_WRESTLER_REQUEST}),
+	postWrestlerSuccess: (wrestler) => ({
+		type: actionTypes.POST_WRESTLER_SUCCESS,
+		wrestler,
+	}),
+	postWrestlerFailure: (error) => ({
+		type: actionTypes.POST_WRESTLER_FAILURE,
+		error,
+	}),
+	putWrestlerRequest: () => ({ type: actionTypes.PUT_WRESTLER_REQUEST}),
+	putWrestlerSuccess: (wrestler) => ({
+		type: actionTypes.PUT_WRESTLER_SUCCESS,
+		wrestler,
+	}),
+	putWrestlerFailure: (error) => ({
+		type: actionTypes.PUT_WRESTLER_FAILURE,
+		error,
+	}),
 };
--- src/actions/wrestlers/actionCreators.spec.js
+++ src/actions/wrestlers/actionCreators.spec.js
@@ -2,21 +2,6 @@ import actionCreators from './actionCreators';
 import actionTypes from './actionTypes';
 
 describe('wrestlers action creators', () => {
-	describe('addWrestler', () => {
-		test('creates the expected action.', () => {
-			const wrestler = {
-				id: 1,
-				name: 'Ravishing Rick Rude',
-			};
-			const expectedAction = {
-				type: actionTypes.ADD_WRESTLER,
-				wrestler,
-			};
-
-			expect(actionCreators.addWrestler(wrestler)).toEqual(expectedAction);
-		});
-	});
-
 	describe('setWrestlers', () => {
 		test('creates the expected action.', () => {
 			const wrestlers = [
@@ -38,18 +23,131 @@ describe('wrestlers action creators', () => {
 		});
 	});
 
-	describe('updateWrestler', () => {
+	describe('postMoveRequest', () => {
+		test('creates the expected action.', () => {
+			const expectedAction = { type: actionTypes.POST_MOVE_REQUEST };
+			expect(actionCreators.postMoveRequest()).toEqual(expectedAction);
+		});
+	});
+
+	describe('postMoveSuccess', () => {
+		test('creates the expected action.', () => {
+			const move = {
+				id: 1,
+				description: 'Rude Awakening',
+			};
+			const expectedAction = {
+				type: actionTypes.POST_MOVE_SUCCESS,
+				move,
+			};
+			expect(actionCreators.postMoveSuccess(move)).toEqual(expectedAction);
+		});
+	});
+
+	describe('postMoveFailure', () => {
+		test('creates the expected action.', () => {
+			const error = new Error();
+			const expectedAction = {
+				type: actionTypes.POST_MOVE_FAILURE,
+				error,
+			};
+			expect(actionCreators.postMoveFailure(error)).toEqual(expectedAction);
+		});
+	});
+
+	describe('putMoveRequest', () => {
+		test('creates the expected action.', () => {
+			const expectedAction = { type: actionTypes.PUT_MOVE_REQUEST };
+			expect(actionCreators.putMoveRequest()).toEqual(expectedAction);
+		});
+	});
+
+	describe('putMoveSuccess', () => {
+		test('creates the expected action.', () => {
+			const move = {
+				id: 2,
+				description: 'Perfect-plex',
+			};
+			const expectedAction = {
+				type: actionTypes.PUT_MOVE_SUCCESS,
+				move,
+			};
+			expect(actionCreators.putMoveSuccess(move)).toEqual(expectedAction);
+		});
+	});
+
+	describe('putMoveFailure', () => {
+		test('creates the expected action.', () => {
+			const error = new Error();
+			const expectedAction = {
+				type: actionTypes.PUT_MOVE_FAILURE,
+				error,
+			};
+			expect(actionCreators.putMoveFailure(error)).toEqual(expectedAction);
+		});
+	});
+
+	describe('postWrestlerRequest', () => {
+		test('creates the expected action.', () => {
+			const expectedAction = { type: actionTypes.POST_WRESTLER_REQUEST };
+			expect(actionCreators.postWrestlerRequest()).toEqual(expectedAction);
+		});
+	});
+
+	describe('postWrestlerSuccess', () => {
 		test('creates the expected action.', () => {
 			const wrestler = {
 				id: 1,
 				name: 'Ravishing Rick Rude',
 			};
 			const expectedAction = {
-				type: actionTypes.UPDATE_WRESTLER,
+				type: actionTypes.POST_WRESTLER_SUCCESS,
 				wrestler,
 			};
+			expect(actionCreators.postWrestlerSuccess(wrestler)).toEqual(expectedAction);
+		});
+	});
 
-			expect(actionCreators.updateWrestler(wrestler)).toEqual(expectedAction);
+	describe('postWrestlerFailure', () => {
+		test('creates the expected action.', () => {
+			const error = new Error();
+			const expectedAction = {
+				type: actionTypes.POST_WRESTLER_FAILURE,
+				error,
+			};
+			expect(actionCreators.postWrestlerFailure(error)).toEqual(expectedAction);
+		});
+	});
+
+	describe('putWrestlerRequest', () => {
+		test('creates the expected action.', () => {
+			const expectedAction = { type: actionTypes.PUT_WRESTLER_REQUEST };
+			expect(actionCreators.putWrestlerRequest()).toEqual(expectedAction);
+		});
+	});
+
+	describe('putWrestlerSuccess', () => {
+		test('creates the expected action.', () => {
+			const wrestler = {
+				id: 2,
+				name: 'Mr. Perfect',
+			};
+			const expectedAction = {
+				type: actionTypes.PUT_WRESTLER_SUCCESS,
+				wrestler,
+			};
+			expect(actionCreators.putWrestlerSuccess(wrestler)).toEqual(expectedAction);
+		});
+	});
+
+	describe('putWrestlerFailure', () => {
+		test('creates the expected action.', () => {
+			const error = new Error();
+			const expectedAction = {
+				type: actionTypes.PUT_WRESTLER_FAILURE,
+				error,
+			};
+			expect(actionCreators.putWrestlerFailure(error)).toEqual(expectedAction);
 		});
 	});
 });
