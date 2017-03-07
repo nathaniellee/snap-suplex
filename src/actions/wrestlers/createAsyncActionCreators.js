9b163d6097ff5b2afb71a12fb2d5072f477725f6
Nathaniel Lee <nlee@appnexus.com>
2017-03-07 00:31:53 -0500

    Cleaned up the async action creator code a bit. Added unit tests for update wrestler async action creator.

--- src/actions/wrestlers/createAsyncActionCreators.js
+++ src/actions/wrestlers/createAsyncActionCreators.js
@@ -4,57 +4,31 @@ import createFetchActionCreators from '../fetch/createAsyncActionCreators';
 
 export default (api) => ({
 	addWrestler: (wrestler) => (dispatch) => {
-		const onPostMoveSuccess = (postedMove) => {
-			dispatch(actionCreators.postMoveSuccess(postedMove));
-			return postedMove;
+		const onPostMoveSuccess = (move) => {
+			dispatch(actionCreators.postMoveSuccess(move));
+			return move;
 		};
 		const onPostMoveFailure = (error) => {
 			dispatch(actionCreators.postMoveFailure(error));
 			return Promise.reject(error);
 		};
-
-		const braMovePostPromises = _.map(wrestler.moves.bra, (move) => {
+		const getMovePromise = (move) => {
 			dispatch(actionCreators.postMoveRequest());
 			return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
-		});
-		const braMovePostsDonePromise = Promise.all(braMovePostPromises)
-			.then((braMoves) => ({ bra: _.map(braMoves, 'id') }));
+		};
 
-		const dexMovePostPromises = _.map(wrestler.moves.dex, (move) => {
-			dispatch(actionCreators.postMoveRequest());
-			return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
+		const movePromises = _.map([
+			'bra',
+			'dex',
+			'str',
+			'tec',
+			'fin',
+		], (stat) => {
+			return Promise.all(_.map(wrestler.moves[stat], getMovePromise))
+				.then((moves) => ({ [stat]: _.map(moves, 'id') }));
 		});
-		const dexMovePostsDonePromise = Promise.all(dexMovePostPromises)
-			.then((dexMoves) => ({ dex: _.map(dexMoves, 'id') }));
 
-		const strMovePostPromises = _.map(wrestler.moves.str, (move) => {
-			dispatch(actionCreators.postMoveRequest());
-			return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
-		});
-		const strMovePostsDonePromise = Promise.all(strMovePostPromises)
-			.then((strMoves) => ({ str: _.map(strMoves, 'id') }));
-
-		const tecMovePostPromises = _.map(wrestler.moves.tec, (move) => {
-			dispatch(actionCreators.postMoveRequest());
-			return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
-		});
-		const tecMovePostsDonePromise = Promise.all(tecMovePostPromises)
-			.then((tecMoves) => ({ tec: _.map(tecMoves, 'id') }));
-
-		const finisherPostPromises = _.map(wrestler.moves.fin, (move) => {
-			dispatch(actionCreators.postMoveRequest());
-			return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
-		});
-		const finisherPostsDonePromise = Promise.all(finisherPostPromises)
-			.then((finishers) => ({ fin: _.map(finishers, 'id') }));
-
-		return Promise.all([
-			braMovePostsDonePromise,
-			dexMovePostsDonePromise,
-			strMovePostsDonePromise,
-			tecMovePostsDonePromise,
-			finisherPostsDonePromise,
-		]).then((moves) => {
+		return Promise.all(movePromises).then((moves) => {
 			const newWrestler = {
 				...wrestler,
 				moves: _.reduce(moves, (results, statMoves) => ({
@@ -75,43 +49,44 @@ export default (api) => ({
 	},
 
 	updateWrestler: (wrestler) => (dispatch) => {
-		const braMovePostPromises = _.map(wrestler.moves.bra, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const braMovePostsDonePromise = Promise.all(braMovePostPromises)
-			.then((braMoves) => ({ bra: _.map(braMoves, 'id') }));
+		const onPostMoveSuccess = (move) => {
+			dispatch(actionCreators.postMoveSuccess(move));
+			return move;
+		};
+		const onPostMoveFailure = (error) => {
+			dispatch(actionCreators.postMoveFailure(error));
+			return Promise.reject(error);
+		};
+		const onPutMoveSuccess = (move) => {
+			dispatch(actionCreators.putMoveSuccess(move));
+			return move;
+		};
+		const onPutMoveFailure = (error) => {
+			dispatch(actionCreators.putMoveFailure(error));
+			return Promise.reject(error);
+		};
+		const getMovePromise = (move) => {
+			if (_.isString(move.id)) {
+				dispatch(actionCreators.postMoveRequest());
+				return api.moves.post(move).then(onPostMoveSuccess, onPostMoveFailure);
+			} else {
+				dispatch(actionCreators.putMoveRequest());
+				return api.moves.put(move).then(onPutMoveSuccess, onPutMoveFailure);
+			}
+		};
 
-		const dexMovePostPromises = _.map(wrestler.moves.dex, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const dexMovePostsDonePromise = Promise.all(dexMovePostPromises)
-			.then((dexMoves) => ({ dex: _.map(dexMoves, 'id') }));
+		const movePromises = _.map([
+			'bra',
+			'dex',
+			'str',
+			'tec',
+			'fin',
+		], (stat) => {
+			return Promise.all(_.map(wrestler.moves[stat], getMovePromise))
+				.then((moves) => ({ [stat]: _.map(moves, 'id') }));
+		});
 
-		const strMovePostPromises = _.map(wrestler.moves.str, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const strMovePostsDonePromise = Promise.all(strMovePostPromises)
-			.then((strMoves) => ({ str: _.map(strMoves, 'id') }));
-
-		const tecMovePostPromises = _.map(wrestler.moves.tec, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const tecMovePostsDonePromise = Promise.all(tecMovePostPromises)
-			.then((tecMoves) => ({ tec: _.map(tecMoves, 'id') }));
-
-		const finisherPostPromises = _.map(wrestler.moves.fin, (move) => _.isString(move.id)
-			? api.moves.post(move)
-			: api.moves.put(move));
-		const finisherPostsDonePromise = Promise.all(finisherPostPromises)
-			.then((finishers) => ({ fin: _.map(finishers, 'id') }));
-
-		return Promise.all([
-			braMovePostsDonePromise,
-			dexMovePostsDonePromise,
-			strMovePostsDonePromise,
-			tecMovePostsDonePromise,
-			finisherPostsDonePromise,
-		]).then((moves) => {
+		return Promise.all(movePromises).then((moves) => {
 			const updatedWrestler = {
 				...wrestler,
 				moves: _.reduce(moves, (results, statMoves) => ({
@@ -119,11 +94,15 @@ export default (api) => ({
 					...statMoves,
 				}), {}),
 			};
+			dispatch(actionCreators.putWrestlerRequest());
 			return api.wrestlers.put(updatedWrestler).then(() => {
 				const asyncActionCreators = createFetchActionCreators(api);
+				dispatch(actionCreators.putWrestlerSuccess(updatedWrestler));
 				dispatch(asyncActionCreators.fetchMoves());
 				dispatch(asyncActionCreators.fetchWrestlers());
+			}, (error) => {
+				dispatch(actionCreators.putWrestlerFailure(error));
 			});
-		});
+		}, (error) => {});
 	},
 });
--- src/actions/wrestlers/createAsyncActionCreators.spec.js
+++ src/actions/wrestlers/createAsyncActionCreators.spec.js
@@ -148,4 +148,139 @@ describe('create wrestler action creators', () => {
 			});
 		});
 	});
+
+	describe('updateWrestler', () => {
+		const wrestler = _.head(mockWrestlers);
+
+		test('creates the expected request actions.', () => {
+			const api = {
+				moves: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+				wrestlers: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+			};
+			const { updateWrestler } = createAsyncActionCreators(api);
+
+			return store.dispatch(updateWrestler(wrestler)).then(() => {
+				const actions = store.getActions();
+				const moveCount = _.sum([
+					_.size(wrestler.moves.str),
+					_.size(wrestler.moves.bra),
+					_.size(wrestler.moves.dex),
+					_.size(wrestler.moves.tec),
+					_.size(wrestler.moves.fin),
+				]);
+				const putMoveRequestActions = _.filter(actions, (action) => {
+					return action.type === actionTypes.PUT_MOVE_REQUEST;
+				});
+
+				expect(_.size(putMoveRequestActions)).toEqual(moveCount);
+				expect(_.find(actions, { type: actionTypes.PUT_WRESTLER_REQUEST }))
+					.toBeDefined();
+			});
+		});
+
+		test('creates the expected success action upon move put success.', () => {
+			const api = {
+				moves: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+				wrestlers: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+			};
+			const { updateWrestler } = createAsyncActionCreators(api);
+
+			return store.dispatch(updateWrestler(wrestler)).then(() => {
+				const actions = store.getActions();
+				const moveCount = _.sum([
+					_.size(wrestler.moves.str),
+					_.size(wrestler.moves.bra),
+					_.size(wrestler.moves.dex),
+					_.size(wrestler.moves.tec),
+					_.size(wrestler.moves.fin),
+				]);
+				const putMoveRequestActions = _.filter(actions, (action) => {
+					return action.type === actionTypes.PUT_MOVE_SUCCESS;
+				});
+
+				expect(_.size(putMoveRequestActions)).toEqual(moveCount);
+			});
+		});
+
+		test('creates the expected failure action upon move put failure.', () => {
+			const api = {
+				moves: {
+					put: () => Promise.reject('boo'),
+				},
+			};
+			const { updateWrestler } = createAsyncActionCreators(api);
+
+			return store.dispatch(updateWrestler(wrestler)).then(() => {
+				const actions = store.getActions();
+				const moveCount = _.sum([
+					_.size(wrestler.moves.str),
+					_.size(wrestler.moves.bra),
+					_.size(wrestler.moves.dex),
+					_.size(wrestler.moves.tec),
+					_.size(wrestler.moves.fin),
+				]);
+				const putMoveFailureActions = _.filter(actions, (action) => {
+					return action.type === actionTypes.PUT_MOVE_FAILURE;
+				});
+
+				expect(_.size(putMoveFailureActions)).toEqual(moveCount);
+			});
+		});
+
+		test('creates the expected failure action upon wrestler put failure.', () => {
+			const api = {
+				moves: {
+					put: () => Promise.resolve(),
+				},
+				wrestlers: {
+					put: () => Promise.reject('boo'),
+				},
+			};
+			const { updateWrestler } = createAsyncActionCreators(api);
+
+			return store.dispatch(updateWrestler(wrestler)).then(() => {
+				const actions = store.getActions();
+				const putWrestlerFailureAction = _.find(actions, {
+					type: actionTypes.PUT_WRESTLER_FAILURE,
+				});
+
+				expect(putWrestlerFailureAction).toBeDefined();
+			});
+		});
+
+		test('creates the expected success action upon wrestler put success.', () => {
+			const api = {
+				moves: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+				wrestlers: {
+					get: () => Promise.resolve(),
+					put: () => Promise.resolve(),
+				},
+			};
+			const { updateWrestler } = createAsyncActionCreators(api);
+
+			return store.dispatch(updateWrestler(wrestler)).then(() => {
+				const actions = store.getActions();
+				const putWrestlerSuccessAction = _.find(actions, {
+					type: actionTypes.PUT_WRESTLER_SUCCESS,
+				});
+
+				expect(putWrestlerSuccessAction).toBeDefined();
+			});
+		});
+	});
 });
