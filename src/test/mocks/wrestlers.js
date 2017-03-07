e09a5788b927d9119861f52e15af1ab81621eeb1
Nathaniel Lee <nlee@appnexus.com>
2017-03-07 02:43:44 -0500

    Mock data used by unit tests.

---
+++ src/test/mocks/wrestlers.js
@@ -0,0 +1,128 @@
+export default [
+	{
+		id: 1,
+		name: 'Brock Lesnar',
+		stats: {
+			str: 7,
+			bra: 6,
+			dex: 2,
+			tec: 6,
+			sta: 6,
+		},
+		moves: {
+			str: [
+				{
+					id: 1,
+					description: 'release German suplex'
+				},
+				{
+					id: 2,
+					description: 'belly-to-belly suplex'
+				},
+				{
+					id: 3,
+					description: 'belly-to-back suplex'
+				},
+			],
+			bra: [
+				{
+					id: 4,
+					description: 'repeated knee lifts to the mid-section',
+				},
+				{
+					id: 5,
+					description: 'repeated shoulder thrusts in the corner',
+				},
+				{
+					id: 6,
+					description: 'mounted punches',
+				},
+			],
+			dex: [
+				{
+					id: 7,
+					description: 'shooting star press',
+				},
+			],
+			tec: [
+				{
+					id: 8,
+					description: 'rear naked choke'
+				},
+				{
+					id: 9,
+					description: 'Kimura lock'
+				},
+				{
+					id: 10,
+					description: 'fisherman suplex'
+				},
+			],
+			fin: [
+				{
+					id: 11,
+					description: 'F-5',
+				}
+			],
+		},
+	},
+	{
+		id: 2,
+		name: 'Goldberg',
+		stats: {
+			str: 7,
+			bra: 5,
+			dex: 2,
+			tec: 5,
+			sta: 6,
+		},
+		moves: {
+			str: [
+				{
+					id: 12,
+					description: 'full nelson slam'
+				},
+				{
+					id: 13,
+					description: 'firemans carry slam'
+				},
+				{
+					id: 14,
+					description: 'front power slam'
+				},
+			],
+			bra: [
+				{
+					id: 15,
+					description: 'spear',
+				},
+				{
+					id: 16,
+					description: 'big boot',
+				},
+			],
+			dex: [
+				{
+					id: 17,
+					description: 'top-rope shoulder block',
+				},
+			],
+			tec: [
+				{
+					id: 18,
+					description: 'dragon screw'
+				},
+				{
+					id: 19,
+					description: 'cross arm breaker'
+				},
+			],
+			fin: [
+				{
+					id: 20,
+					description: 'Jackhammer',
+				}
+			],
+		},
+	}
+];
