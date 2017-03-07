e9f5a60ac8c01973cc9a7c1dab2ed44d9ccc6611
Nathaniel Lee <nlee@appnexus.com>
2017-03-07 18:28:31 -0500

    Added state to track which section of the match setup screen is being viewed.

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -5,30 +5,61 @@ import Parameters from './Parameters/ConnectedParameters';
 import Referee from './Referee/ConnectedReferee';
 import './Match.css';
 
+const matchSetupSteps = {
+	parameters: {
+		index: 0,
+		label: 'Parameters',
+		component: <Parameters />,
+	},
+	referee: {
+		index: 1,
+		label: 'Referee',
+		component: <Referee />,
+	},
+	participants: {
+		index: 2,
+		label: 'Participants',
+		component: <p>Hey now!</p>,
+	},
+	strategies: {
+		index: 3,
+		label: 'Strategies',
+		component: <p>Say what?</p>,
+	},
+};
+
 export default React.createClass({
+	getInitialState() {
+		return {
+			step: 'parameters',
+		};
+	},
+
+	onSelectStep(index) {
+		this.setState({
+			step: _.findKey(matchSetupSteps, { index }),
+		});
+	},
+
 	render() {
+		const { step } = this.state;
+
 		return (
 			<div className='Match'>
 				<Accordion
-					selectedIndex={0}
-					onSelect={_.noop}
+					selectedIndex={matchSetupSteps[step].index}
+					onSelect={this.onSelectStep}
 				>
-					<Accordion.Item>
-						<Accordion.Header>Parameters</Accordion.Header>
-						<Parameters />
-					</Accordion.Item>
-					<Accordion.Item>
-						<Accordion.Header>Referee</Accordion.Header>
-						<Referee />
-					</Accordion.Item>
-					<Accordion.Item>
-						<Accordion.Header>Participants</Accordion.Header>
-						Hey now!
-					</Accordion.Item>
-					<Accordion.Item>
-						<Accordion.Header>Strategies</Accordion.Header>
-						Say what?
-					</Accordion.Item>
+					{_.map(matchSetupSteps, ({
+						component,
+						index,
+						label,
+					}) => (
+						<Accordion.Item key={index}>
+							<Accordion.Header>{label}</Accordion.Header>
+							{component}
+						</Accordion.Item>
+					))}
 				</Accordion>
 			</div>
 		);
