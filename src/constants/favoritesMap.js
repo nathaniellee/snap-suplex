9d12ea6b64a833b0344520955b7336e1ea61a963
Nathaniel Lee <nlee@appnexus.com>
2017-03-10 22:15:40 -0500

    Added favorites, round level and spot flags constants maps.

---
+++ src/constants/favoritesMap.js
@@ -0,0 +1,24 @@
+export default {
+	byId: {
+		0: {
+			id: 0,
+			label: '0',
+			value: 0,
+		},
+		1: {
+			id: 1,
+			label: '1',
+			value: 1,
+		},
+		2: {
+			id: 2,
+			label: '2',
+			value: 2,
+		},
+	},
+	allIds: [
+		0,
+		1,
+		2,
+	],
+};
