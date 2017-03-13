6f15a8fdf2905ce20354ef88601ed519277b08ac
Nathaniel Lee <nlee@appnexus.com>
2017-03-13 00:50:39 -0400

    Simply moved the reducer and selector describe calls into a new parent root describe call.

--- src/reducers/root.spec.js
+++ src/reducers/root.spec.js
@@ -29,146 +29,148 @@ const setWrestlersAction = {
   wrestlers: [mockWrestler],
 };
 
-describe('root reducer', () => {
-  let action;
-  let state;
+describe('root', () => {
+  describe('reducer', () => {
+    let action;
+    let state;
 
-  beforeEach(() => {
-    state = reducer(undefined, {});
-  });
+    beforeEach(() => {
+      state = reducer(undefined, {});
+    });
 
-  test('combines the appropriate reducers.', () => {
-    expect(_.xor([
-      'match',
-      'matchSetup',
-      'moves',
-      'wrestlers',
-    ], _.keys(state))).toHaveLength(0);
+    test('combines the appropriate reducers.', () => {
+      expect(_.xor([
+        'match',
+        'matchSetup',
+        'moves',
+        'wrestlers',
+      ], _.keys(state))).toHaveLength(0);
 
-    // Compare results from an action that the `match` reducer handles.
-    action = { type: actionTypes.START_MATCH };
-    state = reducer(state, action);
-    expect(state.match).toEqual(match(state.match, action));
+      // Compare results from an action that the `match` reducer handles.
+      action = { type: actionTypes.START_MATCH };
+      state = reducer(state, action);
+      expect(state.match).toEqual(match(state.match, action));
 
-    // Compare results from an action that the `matchSetup` reducer handles.
-    action = {
-      type: actionTypes.SET_MAX_ROUNDS,
-      maxRounds: 10,
-    };
-    state = reducer(state, action);
-    expect(state.matchSetup).toEqual(matchSetup(state.matchSetup, action));
+      // Compare results from an action that the `matchSetup` reducer handles.
+      action = {
+        type: actionTypes.SET_MAX_ROUNDS,
+        maxRounds: 10,
+      };
+      state = reducer(state, action);
+      expect(state.matchSetup).toEqual(matchSetup(state.matchSetup, action));
 
-    // Compare results from an action that the `wrestlers` reducer handles.
-    state = reducer(state, setWrestlersAction);
-    expect(state.wrestlers).toEqual(wrestlers(state.wrestlers, setWrestlersAction));
-  });
-});
-
-describe('root selectors', () => {
-  let state;
-
-  beforeEach(() => {
-    state = reducer(undefined, {});
-  });
-
-  describe('get', () => {
-    test('returns the appropriate state.', () => {
-      expect(selectors.get(state)).toEqual(state);
+      // Compare results from an action that the `wrestlers` reducer handles.
+      state = reducer(state, setWrestlersAction);
+      expect(state.wrestlers).toEqual(wrestlers(state.wrestlers, setWrestlersAction));
     });
   });
 
-  describe('match', () => {
-    describe('getMatch', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMatch(state)).toEqual(matchSelectors.get(state.match));
-      });
-    });
-  });
+  describe('selectors', () => {
+    let state;
 
-  describe('match setup', () => {
-    describe('getMatchSetup', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMatchSetup(state)).toEqual(matchSetupSelectors.get(state.matchSetup));
-      });
+    beforeEach(() => {
+      state = reducer(undefined, {});
     });
 
-    describe('getMaxRounds', () => {
+    describe('get', () => {
       test('returns the appropriate state.', () => {
-        expect(selectors.getMaxRounds(state)).toEqual(matchSetupSelectors.getMaxRounds(state.matchSetup));
+        expect(selectors.get(state)).toEqual(state);
       });
     });
 
-    describe('getStrategies', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getStrategies(state)).toEqual(matchSetupSelectors.getStrategies(state.matchSetup));
+    describe('match', () => {
+      describe('getMatch', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMatch(state)).toEqual(matchSelectors.get(state.match));
+        });
       });
     });
 
-    describe('getStrategyByWrestlerId', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getStrategyByWrestlerId(state, 1)).toEqual(matchSetupSelectors.getStrategyByWrestlerId(state.matchSetup, 1));
+    describe('match setup', () => {
+      describe('getMatchSetup', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMatchSetup(state)).toEqual(matchSetupSelectors.get(state.matchSetup));
+        });
       });
-    });
-  });
 
-  describe('moves', () => {
-    describe('getMove', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMove(state, 0))
-          .toEqual(movesSelectors.getMove(state.moves, 0));
+      describe('getMaxRounds', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMaxRounds(state)).toEqual(matchSetupSelectors.getMaxRounds(state.matchSetup));
+        });
       });
-    });
 
-    describe('getMoveIds', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMoveIds(state))
-          .toEqual(movesSelectors.getMoveIds(state.moves));
+      describe('getStrategies', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getStrategies(state)).toEqual(matchSetupSelectors.getStrategies(state.matchSetup));
+        });
       });
-    });
 
-    describe('getMovesAsArray', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMovesAsArray(state))
-          .toEqual(movesSelectors.getMovesAsArray(state.moves));
+      describe('getStrategyByWrestlerId', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getStrategyByWrestlerId(state, 1)).toEqual(matchSetupSelectors.getStrategyByWrestlerId(state.matchSetup, 1));
+        });
       });
     });
 
-    describe('getMovesAsMap', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getMovesAsMap(state))
-          .toEqual(movesSelectors.getMovesAsMap(state.moves));
+    describe('moves', () => {
+      describe('getMove', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMove(state, 0))
+            .toEqual(movesSelectors.getMove(state.moves, 0));
+        });
       });
-    });
-  });
 
-  describe('wrestlers', () => {
-    describe('getWrestler', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getWrestler(state, 0))
-          .toEqual({ wrestler: null });
-        expect(selectors.getWrestler(state, mockWrestler.id))
-          .toEqual({ wrestler: wrestlersSelectors.getWrestler(state.wrestlers, mockWrestler.id) });
+      describe('getMoveIds', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMoveIds(state))
+            .toEqual(movesSelectors.getMoveIds(state.moves));
+        });
       });
-    });
 
-    describe('getWrestlerIds', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getWrestlerIds(state))
-          .toEqual(wrestlersSelectors.getWrestlerIds(state.wrestlers));
+      describe('getMovesAsArray', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMovesAsArray(state))
+            .toEqual(movesSelectors.getMovesAsArray(state.moves));
+        });
       });
-    });
 
-    describe('getWrestlersAsArray', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getWrestlersAsArray(state))
-          .toEqual(wrestlersSelectors.getWrestlersAsArray(state.wrestlers));
+      describe('getMovesAsMap', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getMovesAsMap(state))
+            .toEqual(movesSelectors.getMovesAsMap(state.moves));
+        });
       });
     });
 
-    describe('getWrestlersAsMap', () => {
-      test('returns the appropriate state.', () => {
-        expect(selectors.getWrestlersAsMap(state))
-          .toEqual(wrestlersSelectors.getWrestlersAsMap(state.wrestlers));
+    describe('wrestlers', () => {
+      describe('getWrestler', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getWrestler(state, 0))
+            .toEqual({ wrestler: null });
+          expect(selectors.getWrestler(state, mockWrestler.id))
+            .toEqual({ wrestler: wrestlersSelectors.getWrestler(state.wrestlers, mockWrestler.id) });
+        });
+      });
+
+      describe('getWrestlerIds', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getWrestlerIds(state))
+            .toEqual(wrestlersSelectors.getWrestlerIds(state.wrestlers));
+        });
+      });
+
+      describe('getWrestlersAsArray', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getWrestlersAsArray(state))
+            .toEqual(wrestlersSelectors.getWrestlersAsArray(state.wrestlers));
+        });
+      });
+
+      describe('getWrestlersAsMap', () => {
+        test('returns the appropriate state.', () => {
+          expect(selectors.getWrestlersAsMap(state))
+            .toEqual(wrestlersSelectors.getWrestlersAsMap(state.wrestlers));
+        });
       });
     });
   });
