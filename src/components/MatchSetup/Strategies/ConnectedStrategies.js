e98f4084efaf5a3b3e738a4c0fc3045db78e29ee
Nathaniel Lee <nlee@appnexus.com>
2017-03-14 21:03:30 -0400

    Updated where strategies was being pulled from state.

--- src/components/MatchSetup/Strategies/ConnectedStrategies.js
+++ src/components/MatchSetup/Strategies/ConnectedStrategies.js
@@ -7,9 +7,9 @@ import Strategies from './Strategies';
 const mapStateToProps = (state) => {
 	const {
 		maxRounds,
+		strategies,
 		wrestlers: wrestlerIds,
 	} = selectors.getMatchSetup(state);
-	const strategies = selectors.getStrategies(state);
 	const selectedWrestlers = _.map(wrestlerIds, (id) => selectors.getWrestler(state, id).wrestler);
 
 	return {
