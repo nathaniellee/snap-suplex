1e376382c1907a770abc058c7a17000203b466bb
Nathaniel Lee <nlee@appnexus.com>
2017-03-19 13:04:11 -0400

    Added some match utility methods.

--- src/utils/match.js
+++ src/utils/match.js
@@ -11,10 +11,31 @@ const healthLevelsMap = {
 	8: [40, 24, 12, 4],
 };
 
+const toHitModifierMap = {
+	1: [0, -1, -2, -3],
+	2: [0, 0, -1, -2],
+	3: [0, 0, 0, -1],
+	4: [0, 0, 0, 0],
+};
+
+// Returns the starting health for the provided stamina.
 export const getInitialHealth = (stamina) => _.chain(healthLevelsMap)
 	.get(stamina)
 	.head()
 	.value();
+
+// Returns the health level for the provided stamina and health values.
+export const getHealthLevel = (stamina, health) => {
+	const thresholds = _.get(healthLevelsMap, stamina);
+	const index = _.findLastIndex(thresholds, (threshold) => (health <= threshold));
+	return index + 1;
+};
+
+// Returns the to-hit modifier for the provided health level and round level.
+export const getToHitModifier = (healthLevel, roundLevel) => {
+	const modifiers = _.get(toHitModifierMap, healthLevel);
+	return _.nth(modifiers, roundLevel);
+};
 
 // Simulate the roll of a 10-sided die.
 const roll = () => _.random(1, 10);
@@ -22,12 +43,13 @@ const roll = () => _.random(1, 10);
 // Returns whether a roll is successful against the provided stat value.
 const statRoll = (stat) => (roll() <= stat);
 
+// Returns the highest stat value.
 const getHighestStat = (stats) => _.chain(stats)
 	.map(_.identity)
 	.max()
 	.value();
 
-// Returns the IDs of the winner and loser of the initiative rolls.
+// Returns the winner and loser of the initiative rolls.
 export const getInitiative = (wrestlers) => {
 	const results = _.chain(wrestlers)
 		.map(({
@@ -44,11 +66,52 @@ export const getInitiative = (wrestlers) => {
 		.value();
 
 	if (_.size(results) === 1) {
+		const winnerId = _.head(results).id;
+		const loserId = _.chain(wrestlers)
+			.keys()
+			.map(_.toNumber)
+			.without(winnerId)
+			.head()
+			.value();
 		return {
-			winner: _.head(results).id,
-			loser: _.last(results).id,
+			winnerId,
+			loserId,
 		};
 	}
 
 	return getInitiative(wrestlers);
+};
+
+// Returns true if the attacker won the round and false if the defender won.
+export const getToHitResults = ({
+  attackerStat,
+  attackerToHitModifier,
+  defenderStat,
+  defenderToHitModifier,
+}) => {
+	const attackerSucceeded = statRoll(attackerStat + attackerToHitModifier);
+	const defenderSucceeded = statRoll(defenderStat + defenderToHitModifier);
+
+	if (attackerSucceeded) {
+		return {
+			attackerWon: true,
+			attackerSucceeded,
+			defenderSucceeded,
+		};
+	}
+
+	if (defenderSucceeded) {
+		return {
+			attackerWon: false,
+			attackerSucceeded,
+			defenderSucceeded,
+		};
+	}
+
+	return getToHitResults({
+	  attackerStat,
+	  attackerToHitModifier,
+	  defenderStat,
+	  defenderToHitModifier,
+	});
 };
