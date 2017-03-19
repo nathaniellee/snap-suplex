7af45e31f6850bc573abae2fc5b5c4fadb6cc6cb
Nathaniel Lee <nlee@appnexus.com>
2017-03-19 16:53:07 -0400

    Incorporated pin attempt results.

--- src/components/Match/RoundSummary/RoundSummary.js
+++ src/components/Match/RoundSummary/RoundSummary.js
@@ -1,19 +1,18 @@
+import _ from 'lodash';
 import { Panel } from 'lucid';
 import React from 'react';
 import './RoundSummary.css';
 
 const {
-	bool,
 	number,
 	object,
 	string,
 } = React.PropTypes;
 
 const RoundSummary = ({
-	attemptPin = false,
-	attemptSubmission = false,
 	damage = null,
 	loser,
+	numPinAttemptFailures = null,
 	roundNumber = 1,
 	targetStat,
 	winner,
@@ -22,11 +21,20 @@ const RoundSummary = ({
 
 	if (damage) {
 		description = `${winner.name} inflicted ${damage} damage to ${loser.name}.`;
-		if (attemptPin) {
-			description = `${description} ${winner.name} makes the cover...`;
-		}
-		if (attemptSubmission) {
-			description = `${description} The referee checks for submission...`;
+		if (_.isNumber(numPinAttemptFailures)) {
+			description = `${description} ${winner.name} makes the cover!`;
+			if (numPinAttemptFailures === 0) {
+				description = `${description} And ${loser.name} kicks out right away!`;
+			}
+			if (numPinAttemptFailures === 1) {
+				description = `${description} 1... and a kick out!`;
+			}
+			if (numPinAttemptFailures === 2) {
+				description = `${description} 1... 2... and ${loser.name} just manages to get the shoulder up!`;
+			}
+			if (numPinAttemptFailures === 3) {
+				description = `${description} 1... 2... 3! DING DING DING!!`;
+			}
 		}
 	} else {
 		description = `${winner.name} did not inflict any damage to ${loser.name}.`;
@@ -43,10 +51,9 @@ const RoundSummary = ({
 };
 
 RoundSummary.propTypes = {
-	attemptPin: bool,
-	attemptSubmission: bool,
 	damage: number,
 	loser: object.isRequired,
+	numPinAttemptFailures: number,
 	roundNumber: number,
 	targetStat: string,
 	winner: object.isRequired,
